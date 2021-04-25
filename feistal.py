db359b85775049f97e2e655e7fca7ad83d01b476
Radhika <radhika@cs.iitr.ac.in>
2021-04-25 19:14:52 +0530

    Implement Feistal Cyrptosystem for the defined problem statement

---
+++ feistal.py
@@ -0,0 +1,52 @@
+from Crypto.Util.strxor import strxor
+from Crypto.Util.Padding import pad
+from random import getrandbits 
+
+KEY_LENGTH = 12
+BLOCK_SIZE = 8
+NUM_ROUNDS = 3
+
+def extract_bits(num: int, lower_limit, upper_limit):
+
+    return num&()
+
+def feistel_block(plaintext: bytearray, key: bytearray):
+    # print(f'PT: {plaintext}')
+    left_array = plaintext[4:8]
+    right_array = plaintext[0:4]
+
+    round_keys = list()
+    
+    round_keys.append(((key&0xf0)>>4) ^ ((key&0xf00)>>8))
+    round_keys.append(key&0xf ^ ((key&0xf0)>>4))
+    round_keys.append(key&0xf ^ ((key&0xf00)>>8))
+    # print(round_keys)
+    for i in range(NUM_ROUNDS):
+        left_array_new = right_array
+        right_array = bin(int(left_array, 2)^round_keys[i] ).strip('0b')
+        left_array = left_array_new
+    # print(f'LEFT: {left_array} RIGHT: {right_array}')
+    ciphertext_block = left_array + right_array
+    return ciphertext_block
+
+def feistel_system_3(plaintext, key):
+    
+    ciphertext = ''
+    for i in range(int(len(plaintext)/8)):
+        plaintext_block = plaintext[8*i:8*i+8]
+        ciphertext_block = feistel_block(plaintext_block, key)
+        ciphertext += chr(int(ciphertext_block,2))
+    print(repr(ciphertext))
+
+def main():
+
+    plaintext = bytes(input("Please enter your plaintext").strip('\n'), encoding='utf-8')
+    plaintext = bin(int(plaintext.hex(), 16)).strip('0b')
+    plaintext = plaintext.zfill(len(plaintext) + 8-len(plaintext)%8)
+    key = getrandbits(12)
+    print(f'KEY: {key}')
+    feistel_system_3(plaintext, key)
+
+if __name__ == '__main__':
+    main()
+
