8a1b735215d33b5b2f7bb0eca4890844cbc569f1
Radhika <radhika@cs.iitr.ac.in>
2021-04-25 20:50:33 +0530

    Add substitution box to previously implemented feistal and add fixes

--- sbox_feistal.py
+++ sbox_feistal.py
@@ -2,19 +2,39 @@ from Crypto.Util.strxor import strxor
 from Crypto.Util.Padding import pad
 from random import getrandbits 
 
+
 KEY_LENGTH = 12
 BLOCK_SIZE = 8
 NUM_ROUNDS = 3
 
-def extract_bits(num: int, lower_limit, upper_limit):
+def sbox(state: int):
+    switcher = {
+        0x0: 0xC,
+        0x1: 0x5,
+        0x2: 0x6,
+        0x3: 0xB,
+        0x4: 0x9,
+        0x5: 0x0, 
+        0x6: 0xA,
+        0x7: 0xD,
+        0x8: 0x3,
+        0x9: 0xE,
+        0xA: 0xF,
+        0xB: 0x8,
+        0xC: 0x4,
+        0xD: 0x7,
+        0xE: 0x1,
+        0xF: 0x2,
+    }
+    return switcher.get(state)
 
-    return num&()
 
-def feistel_block(plaintext: bytearray, key: bytearray):
+def sbox_feistel_block(plaintext: bytearray, key: bytearray):
     # print(f'PT: {plaintext}')
     left_array = plaintext[4:8]
     right_array = plaintext[0:4]
-
+    left = int(left_array, 2)
+    right = int(right_array, 2)
     round_keys = list()
     
     round_keys.append(((key&0xf0)>>4) ^ ((key&0xf00)>>8))
@@ -22,21 +42,29 @@ def feistel_block(plaintext: bytearray, key: bytearray):
     round_keys.append(key&0xf ^ ((key&0xf00)>>8))
     # print(round_keys)
     for i in range(NUM_ROUNDS):
-        left_array_new = right_array
-        right_array = bin(int(left_array, 2)^round_keys[i] ).strip('0b')
-        left_array = left_array_new
-    # print(f'LEFT: {left_array} RIGHT: {right_array}')
+        left_new = right
+        right = left^round_keys[i]
+        left = left_new
+        if i < NUM_ROUNDS - 1:
+            left = sbox(left)
+            right = sbox(right)
+    left_array = bin(left).strip('0b')
+    right_array = bin(right).strip('0b')
+    print(f'LEFT: {left_array} RIGHT: {right_array}')
     left_array = left_array.zfill(len(left_array) + 4-len(left_array)%5)
     right_array = right_array.zfill(len(right_array) + 4-len(right_array)%5)
+    print('zfill vaala')
+    print(f'LEFT: {left_array} RIGHT: {right_array}')
     ciphertext_block = left_array + right_array
     return ciphertext_block
 
-def feistel_system_3(plaintext, key):
+
+def sbox_feistel_system_3(plaintext, key):
     
     ciphertext = ''
     for i in range(int(len(plaintext)/8)):
         plaintext_block = plaintext[8*i:8*i+8]
-        ciphertext_block = feistel_block(plaintext_block, key)
+        ciphertext_block = sbox_feistel_block(plaintext_block, key)
         ciphertext += chr(int(ciphertext_block,2))
     print(repr(ciphertext))
 
@@ -47,8 +75,7 @@ def main():
     plaintext = plaintext.zfill(len(plaintext) + 8-len(plaintext)%9) # Now the plaintext is a string of 0's and 1's only
     key = getrandbits(12)
     print(f'KEY: {key}')
-    feistel_system_3(plaintext, key)
+    sbox_feistel_system_3(plaintext, key)
 
 if __name__ == '__main__':
     main()
-
