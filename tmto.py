84d6ea4a56e9bd63d08b587809f31dc0743d0535
Radhika <radhika@cs.iitr.ac.in>
2021-04-29 02:48:04 +0530

    Reduced list search working

--- tmto.py
+++ tmto.py
@@ -18,6 +18,7 @@ def permute(j:int, x:int):
 
 def compute_tmto_lists(plaintext):
     lists = []
+    matrices = []
     sp = sample(range(0, 2**8), m)
     # for i in range(m):
     #     el = bytearray(bin(sp[i]).strip('0b').zfill(8), encoding='utf-8')
@@ -40,53 +41,40 @@ def compute_tmto_lists(plaintext):
             assert matrix[i][0] not in l , f'{i}, {l_num}'
             l[matrix[i][0]] = matrix[i][t-1]
         # print(len(l))
-        lists.append(matrix)
-    return lists
+        lists.append(l)
+        matrices.append(matrix)
+    return lists, matrices
 
 def get_key(plaintext:bytearray, ciphertext: bytearray):
-    lists = compute_tmto_lists(plaintext)
-    for precomputed_matrix in lists:
+    lists, matrices = compute_tmto_lists(plaintext)
+    indices = []
+    mat_num = 0
+    for precomputed_matrix in matrices:
         for i in range(m):
             for j in range(t):
                 if ciphertext == precomputed_matrix[i][j] % (2**8)   and j != 0:
+                    indices = [i, j]
+                    break
                     # print(f"i: {i}, j:{j}, matrix element: {precomputed_matrix[i][j]}")
-                    return precomputed_matrix[i][j-1]
-    
-    # assert(len(lists) == 16)
-    # for i in range(t):
-    #     print(i)
-    #     assert(len(lists[i]) == 16)
+                    # return precomputed_matrix[i][j-1]
+        mat_num += 1
 
-    # for i in range(t):
-    #     num_to_append = (i << 8)
-    #     for j in range(t):
-    #         c = num_to_append + ciphertext
-    #         c_i = c
-    #         for x in range(i):
-    #             c_i = num_to_append + sbox_feistel_block(plaintext, c_i)
-    #         for l in range(m):
-    #             for k, value in lists[j].items():
-    #                 if c_i == value:
-    #                     x_l0 = k
-    #                     key = x_l0
-    #                     y = key
-    #                     for i in range(t-1):
-    #                         y = num_to_append + sbox_feistel_block(plaintext, y)
-    #                     assert( value == y)
-    #                     for x in range(t- i - 2):
-    #                         key = num_to_append + sbox_feistel_block(plaintext, key)
-    #                     y = key
-    #                     for x in range(i+1):
-    #                         y = num_to_append + sbox_feistel_block(plaintext, y)
-    #                     assert( value == y )
-    #                     ciphertext_expected = sbox_feistel_block(plaintext, key)
-    #                     c_expected = num_to_append + ciphertext_expected
-    #                     for x in range(i):
-    #                         c_expected = num_to_append + sbox_feistel_block(plaintext, c_expected)
-    #                     assert( value == c_expected )
-    #                     print(ciphertext, ciphertext_expected)
-    #                     assert(ciphertext == ciphertext_expected)
-    #                     return key
+    assert(len(lists) == 16)
+    for i in range(t):
+        print(i)
+        assert(len(lists[i]) == 16)
+
+    for i in range(t):
+        num_to_append = (i << 8)
+        c = num_to_append + ciphertext
+        for row in range(m):
+            for k, value in lists[i].items():
+                if c == value:
+                    x_l0 = k
+                    key = x_l0
+                    for x in range(t - 2):
+                        key = num_to_append + sbox_feistel_block(plaintext, key)
+                    return key
     return False                
 
 if __name__=="__main__":
